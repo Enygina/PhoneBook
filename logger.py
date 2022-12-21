6e2ae1541aa3524d7219c7c2ddd41d7a50200b4c
Doctor-TVER <ilyinya@yandex.ru>
2022-12-21 08:07:21 +0300

    add logger, recording

--- logger.py
+++ logger.py
@@ -2,4 +2,11 @@
 # - <Дата и время совершения операции>,
 # - введенные данные
 
-# def log_data()
+from datetime import datetime as dt
+
+
+def log_data(data):
+    time = dt.now().strftime('%d.%m.%y %H:%M')
+    with open('log.csv', 'a') as f:
+        f.write('{}; {}\n'.format(time, data))
+
